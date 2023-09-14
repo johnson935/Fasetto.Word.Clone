71c92822dbcd6ae01c5f60a9f9629bb4116e6548
johnson <johnson935@hotmail.co.uk>
2023-09-14 13:27:46 +0100

    tutorial 10

---
+++ Fasetto.Word/ViewModel/Chat/ChatListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Fasetto.Word.ViewModel.Base;
+using System.Collections.Generic;
+
+namespace Fasetto.Word.ViewModel.Chat
+{
+    /// <summary>
+    /// A view model for the overview chat list
+    /// </summary>
+    public class ChatListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat list items for the list
+        /// </summary>
+        public List<ChatListItemViewModel> Items { get; set; }
+    }
+}
